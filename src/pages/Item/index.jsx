406463b47bc87f49a6dc1eaa0a888d527f9f645d
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-07-11 06:14:03 +0530

    Update Item Page

--- src/pages/Item/index.jsx
+++ src/pages/Item/index.jsx
@@ -1,4 +1,4 @@
-import {Component,Fragment} from "react";
+import {Component} from "react";
 import {styleSheet} from "../Item/styles";
 import {withStyles} from "@mui/styles";
 import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography} from "@mui/material";
@@ -8,52 +8,87 @@ import Autocomplete from '@mui/material/Autocomplete';
 import GDSEButton from "../../components/common/Button"
 import Paper from '@mui/material/Paper';
 
-class Item extends Component{
+class Item extends Component {
     constructor(props) {
         super(props);
         this.state = {
             packSize: [
-                { label: 'Large'},
-                { label: 'Medium'},
-                { label: 'Small'},
-            ]
+                {label: 'Large'},
+                {label: 'Medium'},
+                {label: 'Small'},
+            ],
+
+            formData:{
+                code:'',
+                name:'',
+                price:'',
+                packSize:'',
+                description:'',
+                qtyOnHand:''
+            }
         }
+
+    }
+
+    saveItem(){
+
     }
 
     render() {
 
-        function createData(code, name, price, packSize,description, qtyOnHand) {
-            return {code, name, price, packSize,description, qtyOnHand };
+        function createData(code, name, price, packSize, description, qtyOnHand) {
+            return {code, name, price, packSize, description, qtyOnHand};
         }
 
         const {classes} = this.props
         const rows = [
-            createData('I00-001','Frozen yoghurt', 150.0, 'Large', 'AAAAAA', 4),
-            createData('I00-002','Ice cream sandwich', 200.0, 'Small', 'BBBBB', 3),
-            createData('I00-003','Eclair', 350.0, 16.0, 'CCCCC', 6),
-            createData('I00-004','Cupcake', 450.0, 'Medium', 'DDDDDD', 2),
-            createData('I00-005','Gingerbread', 500.0, 'Large', 'EEEEEEE', 9),
+            createData('I00-001', 'Frozen yoghurt', 150.0, 'Large', 'AAAAAA', 4),
+            createData('I00-002', 'Ice cream sandwich', 200.0, 'Small', 'BBBBB', 3),
+            createData('I00-003', 'Eclair', 350.0, 16.0, 'CCCCC', 6),
+            createData('I00-004', 'Cupcake', 450.0, 'Medium', 'DDDDDD', 2),
+            createData('I00-005', 'Gingerbread', 500.0, 'Large', 'EEEEEEE', 9),
         ];
-        return(
-           <div className={classes.container}>
+        return (
+            <div className={classes.container}>
                 <div className={classes.title_bar}>
                     <Typography variant="h4">
                         Item Manage
                     </Typography>
                 </div>
-               <div className={classes.text_form}>
+                <div className={classes.text_form}>
                     <div className={classes.text_field}>
                         <Grid container spacing={3}>
-                            <Grid item lg={6} md={6} sm={6} xm={6} >
-                                <TextField id="outlined-basic" placeHolder="Item Code" label="Item Code" variant="outlined" size="small"
+                            <Grid item lg={6} md={6} sm={6} xm={6}>
+                                <TextField id="outlined-basic" placeHolder="Item Code" label="Item Code"
+                                           variant="outlined" size="small"
+                                           onChange={(e) => {
+                                               console.log(e.target.value)
+                                               let formData = this.state.formData
+                                               formData.code = e.target.value
+                                               this.setState({ formData })
+                                           }}
                                            style={{width: '100%'}}/>
                             </Grid>
-                            <Grid item lg={6} md={6} sm={6} xm={6} >
-                                <TextField id="outlined-basic" placeHolder="Name" label="Item name" variant="outlined" size="small"
-                                           style={{width: '100%'}} />
+                            <Grid item lg={6} md={6} sm={6} xm={6}>
+                                <TextField id="outlined-basic" placeHolder="Name" label="Item name" variant="outlined"
+                                           size="small"
+                                           onChange={(e) =>{
+                                               console.log(e.target.value)
+                                               let formData = this.state.formData
+                                               formData.name = e.target.value
+                                               this.setState({ formData })
+                                           }}
+                                           style={{width: '100%'}}/>
                             </Grid>
                             <Grid item lg={6} md={6} sm={6} xm={6}>
-                                <TextField id="outlined-basic" placeHolder="Price" label="Price" variant="outlined" size="small"
+                                <TextField id="outlined-basic" placeHolder="Price" label="Price" variant="outlined"
+                                           size="small"
+                                           onChange={(e) =>{
+                                               console.log(e.target.value)
+                                               let formData = this.state.formData
+                                               formData.price = e.target.value
+                                               this.setState({ formData })
+                                           }}
                                            style={{width: '100%'}}
                                 />
                             </Grid>
@@ -62,35 +97,57 @@ class Item extends Component{
                                     disablePortal
                                     id="combo-box-demo"
                                     options={this.state.packSize}
-                                    sx={{ width: 300 }}
-                                    renderInput={(params) => <TextField {...params} label="Pack Size" />}
+                                    sx={{width: 300}}
+                                    renderInput={(params) => <TextField {...params} label="Pack Size"/>}
                                     getOptionLabel={
                                         (option) => option.label
                                     }
                                     onChange={(e, value) => {
-                                        console.log(value.label + " " + value.year);
+                                        console.log(e.target.value)
+                                        let formData = this.state.formData
+                                        formData.packSize = e.target.valueOf()
+                                        this.setState({ formData })
                                     }}
                                     size="small"
-                                    style={{ width: '100%' }}
+                                    style={{width: '100%'}}
                                 />
                             </Grid>
                             <Grid item lg={6} md={6} sm={6} xm={6}>
-                                <TextField id="outlined-basic" placeHolder="Description" label="Description" variant="outlined" size="small"
+                                <TextField id="outlined-basic" placeHolder="Description" label="Description"
+                                           variant="outlined" size="small"
+                                           onChange={(e) =>{
+                                               console.log(e.target.value)
+                                               let formData = this.state.formData
+                                               formData.description = e.target.value
+                                               this.setState({ formData })
+                                           }}
                                            style={{width: '100%'}}
                                 />
                             </Grid>
-                            <Grid item lg={6} md={6} sm={6} xm={6} >
-                                <TextField id="outlined-basic" placeHolder="Qty On Han" label="Qty On Hand" variant="outlined" size="small"
+                            <Grid item lg={6} md={6} sm={6} xm={6}>
+                                <TextField id="outlined-basic" placeHolder="Qty On Hand" label="Qty On Hand"
+                                           variant="outlined" size="small"
+                                           onChange={(e) =>{
+                                               console.log(e.target.value)
+                                               let formData = this.state.formData
+                                               formData.qtyOnHand = e.target.value
+                                               this.setState({ formData })
+                                           }}
                                            style={{width: '100%'}}/>
                             </Grid>
-                            <Grid item lg={12} md={12} sm={12} xm={12} style={{display: 'flex'}} justifyContent="flex-end" >
-                                <GDSEButton size="small" variant="contained" label="save"/>
+                            <Grid item lg={12} md={12} sm={12} xm={12} style={{display: 'flex'}}
+                                  justifyContent="flex-end">
+                                <GDSEButton size="small" variant="contained" label="save"
+                                            onClick={() => {
+                                                this.saveItem()
+                                            }}
+                                />
                             </Grid>
                         </Grid>
                     </div>
                     <div className={classes.table_field}>
                         <TableContainer component={Paper}>
-                            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                            <Table sx={{minWidth: 650}} aria-label="simple table">
                                 <TableHead>
                                     <TableRow>
                                         <TableCell>Code&nbsp;</TableCell>
@@ -103,13 +160,8 @@ class Item extends Component{
                                 </TableHead>
                                 <TableBody>
                                     {rows.map((row) => (
-                                        <TableRow
-                                            key={row.name}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                        >
-                                            <TableCell component="th" scope="row">
-                                                {row.code}
-                                            </TableCell>
+                                        <TableRow key={row.name} sx={{'&:last-child td, &:last-child th': {border: 0}}}>
+                                            <TableCell component="th" scope="row">{row.code}</TableCell>
                                             <TableCell align="right">{row.name}</TableCell>
                                             <TableCell align="right">{row.price}</TableCell>
                                             <TableCell align="right">{row.packSize}</TableCell>
@@ -121,8 +173,8 @@ class Item extends Component{
                             </Table>
                         </TableContainer>
                     </div>
-               </div>
-           </div>
+                </div>
+            </div>
         )
     }
 }
